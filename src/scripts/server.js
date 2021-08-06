87846f6dc0d4f3b1119829fdd7055f6e4ab38ce2
Tanner <thetannerthomas@gmail.com>
2021-08-05 20:37:05 -0600

    remove cookie

--- src/scripts/server.js
+++ src/scripts/server.js
@@ -4,7 +4,6 @@ import http from "http";
 import cors from "cors";
 import chalk from "chalk";
 import cookieParser from "cookie-parser";
-import cookieSession from "cookie-session";
 
 import indexRouter from "./routes/index.router";
 import authRouter from "./routes/auth.router";
