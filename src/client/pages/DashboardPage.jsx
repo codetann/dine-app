1f9940f1ae1dd0164a8058a20eb975394d33c15c
Tanner <thetannerthomas@gmail.com>
2021-08-08 13:40:43 -0600

    add transition

--- src/client/pages/DashboardPage.jsx
+++ src/client/pages/DashboardPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { VStack, useBreakpointValue } from "@chakra-ui/react";
 import { useHistory } from "react-router-dom";
 // components
@@ -6,31 +6,26 @@ import MobileNav from "../components/navigation/MobileNav";
 import DesktopNav from "../components/navigation/DesktopNav";
 import Card from "../components/card/Card";
 import AuthPage from "../components/layout/AuthPage";
-
-const STEPS = {
-  HOME: "HOME",
-  JOIN: "JOIN",
-  CREATE: "CREATE",
-  WAITING: "WAITING",
-};
+import FadeTransition from "../components/animations/FadeTransition";
 
 function Dashboard() {
-  const Nav = useBreakpointValue({ base: <MobileNav />, md: <DesktopNav /> });
   const history = useHistory();
-  const [step, setStep] = useState(STEPS.HOME);
 
-  const linkJoinRoom = () => history.push("/joinroom");
-
+  const linkJoinRoom = () => {
+    history.push("/joinroom");
+  };
   return (
     <AuthPage>
       {/* home step */}
 
-      <VStack w="100%">
-        <>
-          <Card onClick={linkJoinRoom} isButton heading="Join Friends" />
-          <Card isButton heading="Create A Room" />
-        </>
-      </VStack>
+      <FadeTransition>
+        <VStack w="100%">
+          <>
+            <Card onClick={linkJoinRoom} isButton heading="Join Friends" />
+            <Card isButton heading="Create A Room" />
+          </>
+        </VStack>
+      </FadeTransition>
     </AuthPage>
   );
 }
