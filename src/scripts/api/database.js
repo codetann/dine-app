4690da07742e8d28ab4678077bce0f162560f4ed
Tanner <thetannerthomas@gmail.com>
2021-08-09 14:13:43 -0600

    fix hash error

--- src/scripts/api/database.js
+++ src/scripts/api/database.js
@@ -17,7 +17,7 @@ export const insertUser = async (name, email, password) => {
 
   if (found) throw new Error("Email already exists");
 
-  const hash = await bcrypt.hash([password, 10]);
+  const hash = await bcrypt.hash(password, 10);
   const user = await User.create({
     name,
     email,
