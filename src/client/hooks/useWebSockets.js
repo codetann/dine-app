c23992f9e8dd81f7c752f102a99a9efc03e92eec
Tanner <thetannerthomas@gmail.com>
2021-08-09 15:05:04 -0600

    add leave-room socket

--- src/client/hooks/useWebSockets.js
+++ src/client/hooks/useWebSockets.js
@@ -6,17 +6,18 @@ const URL = `http://${window.location.hostname}:8050`;
 export default function useWebSockets() {
   const [id, setId] = useState(null);
   const [roomid, setRoomId] = useState(null);
+  const [admin, setAdmin] = useState(false);
   const [error, setError] = useState(null);
   const [socket, setSocket] = useState(null);
   const [members, setMembers] = useState([]);
 
-  // initialize new socket
-  useEffect(() => {
-    const newSocket = io(URL);
-    setSocket(newSocket);
+  // // initialize new socket
+  // useEffect(() => {
+  //   const newSocket = io(URL);
+  //   setSocket(newSocket);
 
-    return () => newSocket.close();
-  }, [setSocket]);
+  //   return () => newSocket.close();
+  // }, [setSocket]);
 
   // set socket listeners after the socket is created
   useEffect(() => {
@@ -31,18 +32,32 @@ export default function useWebSockets() {
         console.log(members);
         setMembers(members);
       });
-      socket.on("success:join-room", ({ id, members }) => {
+      socket.on("success:join-room", ({ id, roomid, members }) => {
         console.log(members);
         setMembers(members);
+        setRoomId(roomid);
         setId(id);
       });
       socket.on("success:create-room", ({ id, members, roomid }) => {
         setMembers(members);
         setId(id);
         setRoomId(roomid);
+        setAdmin(true);
+      });
+      socket.on("new:leave-room", ({ members }) => {
+        setMembers(members);
+      });
+      socket.on("success:leave-room", ({ members }) => {
+        setMembers(members);
+        setRoomId(null);
       });
     }
   }, [socket]);
+
+  const connect = () => {
+    const newSocket = io(URL);
+    setSocket(newSocket);
+  };
 
   // socket.io event handlers
   const joinRoom = (name, roomid) => {
@@ -51,13 +66,19 @@ export default function useWebSockets() {
   const createRoom = (name, details) => {
     socket.emit("create-room", { name, details });
   };
+  const leaveRoom = () => {
+    socket.emit("leave-room");
+  };
 
   return {
     emit: {
       joinRoom,
       createRoom,
+      leaveRoom,
     },
+    connect,
     members,
+    admin,
     id,
     error,
     roomid,
