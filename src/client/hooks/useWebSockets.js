bb5abac86297e78c92ec4bd5dd98e4adeb665457
Tanner <thetannerthomas@gmail.com>
2021-08-09 13:24:37 -0600

    add additional sockets

--- src/client/hooks/useWebSockets.js
+++ src/client/hooks/useWebSockets.js
@@ -1,15 +1,16 @@
 import { useState, useEffect } from "react";
-import { v4 as uuid } from "uuid";
 import io from "socket.io-client";
 
 const URL = `http://${window.location.hostname}:8050`;
 
 export default function useWebSockets() {
-  const [socket, setSocket] = useState(null);
-  const [members, setMembers] = useState(null);
-  const [error, setError] = useState(null);
   const [id, setId] = useState(null);
+  const [roomid, setRoomId] = useState(null);
+  const [error, setError] = useState(null);
+  const [socket, setSocket] = useState(null);
+  const [members, setMembers] = useState([]);
 
+  // initialize new socket
   useEffect(() => {
     const newSocket = io(URL);
     setSocket(newSocket);
@@ -17,27 +18,48 @@ export default function useWebSockets() {
     return () => newSocket.close();
   }, [setSocket]);
 
+  // set socket listeners after the socket is created
   useEffect(() => {
     if (socket) {
-      socket.on("new:join-room", ({ members }) => setMembers(members));
-      socket.on("error:join-room", ({ err }) => setError(err));
+      socket.on("error:any", ({ error }) => {
+        setError(error);
+        setTimeout(() => {
+          setError(null);
+        }, 1000);
+      });
+      socket.on("new:join-room", ({ members }) => {
+        console.log(members);
+        setMembers(members);
+      });
       socket.on("success:join-room", ({ id, members }) => {
+        console.log(members);
         setMembers(members);
         setId(id);
+      });
+      socket.on("success:create-room", ({ id, members, roomid }) => {
+        setMembers(members);
+        setId(id);
+        setRoomId(roomid);
       });
     }
   }, [socket]);
 
-  const joinRoom = (roomid) => {
-    socket.emit("join-room", { id, roomid });
+  // socket.io event handlers
+  const joinRoom = (name, roomid) => {
+    socket.emit("join-room", { name, roomid });
+  };
+  const createRoom = (name, details) => {
+    socket.emit("create-room", { name, details });
   };
 
   return {
     emit: {
       joinRoom,
+      createRoom,
     },
     members,
     id,
     error,
+    roomid,
   };
 }
