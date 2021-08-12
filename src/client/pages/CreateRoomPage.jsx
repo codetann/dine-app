7fc1a0ecf5992e409a9c1b9c4762771d6f3ee9dd
Tanner <thetannerthomas@gmail.com>
2021-08-11 19:58:36 -0600

    add input filters

--- src/client/pages/CreateRoomPage.jsx
+++ src/client/pages/CreateRoomPage.jsx
@@ -1,16 +1,40 @@
-import React from "react";
-import { Button, Heading, VStack } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { Button, Heading, position, VStack } from "@chakra-ui/react";
 import FadeTransition from "../components/animations/FadeTransition";
 import AuthPage from "../components/layout/AuthPage";
+import {
+  SimpleNumberInput,
+  DistanceInput,
+  PriceInput,
+} from "../components/input";
 import { useSockets, useUser } from "../hooks";
 
 export default function CreateRoomPage() {
+  // - State - //
+  const [limitValue, setLimitValue] = useState(10);
+  const [distanceValue, setDistanceValue] = useState(10);
+  const [priceValue, setPriceValue] = useState([1, 2, 3, 4]);
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  // - Hooks - //
   const { createRoom } = useSockets();
   const { user } = useUser();
-
-  // try to create socket
-  const handleCreate = () => createRoom(user.name, "details");
-
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    });
+  }, []);
+  // - Action Handlers - //
+  const handleCreate = () => {
+    console.log(location);
+    createRoom(user.name, {
+      price: priceValue,
+      distance: distanceValue,
+      limit: limitValue,
+      location: { lat: latitude, long: longitude },
+    });
+  };
   return (
     <AuthPage>
       <FadeTransition>
@@ -28,7 +52,25 @@ export default function CreateRoomPage() {
           p="3rem"
           spacing="2rem"
         >
-          <Button onClick={handleCreate} colorScheme="purple">
+          <SimpleNumberInput
+            label="Number of results"
+            value={limitValue}
+            setValue={setLimitValue}
+          />
+
+          <DistanceInput
+            label="Max distance (miles)"
+            value={distanceValue}
+            setValue={setDistanceValue}
+          />
+
+          <PriceInput
+            label="Filter by price"
+            value={priceValue}
+            setValue={setPriceValue}
+          />
+
+          <Button onClick={handleCreate} colorScheme="purple" w="100%">
             Create
           </Button>
         </VStack>
